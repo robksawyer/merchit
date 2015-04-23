6c4cfd994d0725bf48bba8d973f598eb32fc0eec
robksawyer <robksawyer@gmail.com>
2015-04-23 10:35:31 -0700

    Initial commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,18 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	var params = req.params.all()
+
+	User.create({name: params.name}).exec(function createCB(err,created){
+		return res.json({
+			notice: 'Created user with name ' + created.name
+		});
+	});
+
+};
+
